70b2750b1dfbe0d7c9a0a3c9b74d7be68a93112b
Oshin Paul <141218003+ndikjcdnbhdguygidlkfckdkcbdfi@users.noreply.github.com>
2023-12-03 19:09:11 +0530

    Add files via upload

---
+++ multiplication.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Multiplication Table for 7</title>
+</head>
+<body>
+  <script>
+    // JavaScript code to print multiplication table for 7
+    for (let i = 1; i <= 10; i++) {
+      console.log(`7 x ${i} = ${7 * i}`);
+    }
+  </script>
+</body>
+</html>
