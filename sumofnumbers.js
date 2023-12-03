64473c4de314bc3cf3b3ecbb142045a46d276c59
Oshin Paul <141218003+ndikjcdnbhdguygidlkfckdkcbdfi@users.noreply.github.com>
2023-12-03 19:16:15 +0530

    Add files via upload

---
+++ sumofnumbers.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sum of Numbers from 1 to 10</title>
+</head>
+<body>
+
+  <script>
+    let sum = 0;
+    for (let i = 1; i <= 10; i++) {
+      sum += i;
+    }
+    console.log(`Sum of numbers from 1 to 10: ${sum}`);
+  </script>
+
+</body>
+</html>
