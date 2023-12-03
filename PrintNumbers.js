841a51984bd098cd2949b864eefed260c9806cd6
Oshin Paul <141218003+ndikjcdnbhdguygidlkfckdkcbdfi@users.noreply.github.com>
2023-12-03 18:57:35 +0530

    Add files via upload

---
+++ PrintNumbers.js
@@ -0,0 +1,18 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Print 1 to 10 Numbers</title>
+</head>
+<body>
+
+  <script>
+    // JavaScript code to print numbers from 1 to 10
+    for (let i = 1; i <= 10; i++) {
+      console.log(i);
+    }
+  </script>
+
+</body>
+</html>
