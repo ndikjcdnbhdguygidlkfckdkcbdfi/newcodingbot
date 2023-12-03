9ccb98490b612e8e8b5e56e46a62da903fcef3d3
Oshin Paul <141218003+ndikjcdnbhdguygidlkfckdkcbdfi@users.noreply.github.com>
2023-12-03 19:02:51 +0530

    Add files via upload

---
+++ OddNumber.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Print Odd Numbers Less Than 10</title>
+</head>
+<body>
+  <script>
+    for (let i = 1; i < 10; i += 2) {
+      console.log(i);
+    }
+  </script>
+</body>
+</html>
